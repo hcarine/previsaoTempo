6f9c23ebc1bf3e61aeadc0290ea13df7e8637d9a
Carine <hammescarine@gmail.com>
2016-07-25 15:11:01 +0000

    inicial

--- code.js
+++ code.js
@@ -1,4 +1,5 @@
-$.getElement("getClime").click{
+//$.getElement("getClime").click{
     
 }
-<script src="https://query.yahooapis.com/v1/public/yql?q=select wind from weather.forecast where woeid in (select woeid from geo.places(1) where text='chicago, il')&format=json&callback=callbackFunction"></script>
+//script src="https://query.yahooapis.com/v1/public/yql?q=select windfrom weather.forecast where woeid in (select woeid from geo.places(1) where text='chicago, il')&format=json&callback=callbackFunction"></script>
+
