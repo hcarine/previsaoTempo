c36f72fda6835ae3b5b3e9ebdbb4dabdcc4495d8
Carine <hammescarine@gmail.com>
2016-07-26 17:57:41 +0000

    finalização de vai dar praia

--- code.js
+++ code.js
@@ -1,14 +1,20 @@
 $('#getClime').on('submit', function(e) {
     e.preventDefault();
-    getClima();
-});
-
-function getClima(){
     // var city = document.getElementById("city").value.toLowerCase().trim();
     // var state = $("#state").val().toLowerCase();
     var city = "blumenau";
     var state = "santacatarina";
+    getClima(city,state);
+});
 
+function init(){
+    var city = "blumenau";
+    var state = "santacatarina";
+    getClima(city,state);
+}
+
+
+function getClima(city,state){
     $.ajax({
         type: "GET",
         url:'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city +',' + state +',br&mode=json&units=metric&cnt=7&appid=fece2aa20ad50047740813901a13081a',
@@ -19,10 +25,13 @@ function getClima(){
 }
 
 function updatePrevision(dados) {
-    document.getElementById("todayMin").value = dados.list[0].temp.min;
-    document.getElementById("todayMax").value = dados.list[0].temp.max;
-
-    //var showImage = isHotWeekend(dados.list);
+    var min = dados.list[0].temp.min;
+    var max = dados.list[0].temp.min;
+    updateValuesToday(min,max);
+    //city name ===$('h1').html("I have " + count + " list items");
+    
+    var showImage = isHotWeekend(dados.list);
+    showIsBeachDay(showImage);
     //createGraphic(dados);
     
 }
@@ -31,23 +40,42 @@ function messageErro(message){
     console.log("infelismente ocorreu um erro, verifique o nome da cidade e veja se corresponde ao estado");
 }
 
+function updateValuesToday(min, max){
+  document.getElementById("todayMin").value = min;
+  document.getElementById("todayMax").value = max;
+}
+
+
 function createGraphic(dados){
     
 }
 
 function isHotWeekend(days){
     var weekend = [];
-    for(var i; i< days.length; i++){
-        var day = days[i].getDay();
+    for(var i=0; i< days.length; i++){
+        var theDate = new Date(days[i].dt * 1000);
+        var day = theDate.getDay();
        if((day == 6) || (day == 0)){
-           weekend[weekend.length]= day[i].temp.min;
+           weekend[weekend.length] = days[i].temp.max;
        }
     }
-    if(isDayhot(weekend[0])  && isDayhot(weekend[0]) ){
-      return true;  
+    if(isDayhot(weekend[0]) || isDayhot(weekend[1]) ){
+      return true;
     }
+    return false;
 }
 
 function isDayhot(day) {
     return day > 25 ? true : false;
 }
+
+function showIsBeachDay(value){
+    if(value === true){
+        document.getElementById("beach").classList.add('icon-happy-smiley-streamline');
+    }
+    else{
+        document.getElementById("beach").classList.add('icon-frown-o');
+    }
+}
+
+init();
